6df9a9b7ffe70f50430aec8c6a7d46f3f2b75dc2
Abdullah Siddiqui <abdullahsidd37@gmail.com>
2025-01-02 18:10:30 +0500

    moveZerotoend

--- moveZeroToend.js
+++ moveZeroToend.js
@@ -1,20 +1,28 @@
 const moveZeroToEnd = (arr) => {
-    let zeroIndex = 0;
+    // let zeroIndex = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] != 0) {
+    //         if (i != zeroIndex) {
+    //             let temp = arr[i];
+    //             arr[i] = arr[zeroIndex];
+    //             arr[zeroIndex] = temp
+    //         }
+    //         zeroIndex++;
+    //     }
+    // }
+    // return arr
+    let nonZeorIndex = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] != 0) {
-            if (i != zeroIndex) {
-                let temp = arr[i];
-                arr[i] = arr[zeroIndex];
-                arr[zeroIndex] = temp
-            }
-            zeroIndex++;
+            let temp = arr[nonZeorIndex]
+            arr[nonZeorIndex] = arr[i]
+            arr[i] = temp
+            nonZeorIndex++;
         }
     }
-    return arr
+    return arr;
 }
 
-
-const arr = [0, 2, 4, 0, 12, 14, 0, 6, 0, 0.0, 4, 5, 8, 0]
+const arr = [2, 4, 0, 12, 14, 0, 6, 0, 0.0, 4, 5, 8, 0]
 const result = moveZeroToEnd(arr);
 console.log(result);
- 
