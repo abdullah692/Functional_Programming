9b3f78b854ce58c1599abae18752ec5244be4d68
Abdullah Siddiqui <abdullahsidd37@gmail.com>
2025-01-31 16:40:24 +0500

    done

--- ClockAngle.js
+++ ClockAngle.js
@@ -14,19 +14,24 @@ const ClockAngle=(hours,min)=>{
     console.log("hrsMinCal",hrsMinCal);
     
 
+
     let minAngle= min * minCal
+    console.log("minAngle",minAngle);
+    
     let hrsAngle = (hours * hrsCal) + (min * hrsMinCal)
     console.log(hrsAngle,"hrsAngle");
     
 
     let angle=Math.abs(hrsAngle - minAngle)
+    console.log(angle);
+    
 
     if(angle > 180)
     {
-        angle=360-180
+        angle=360-angle
     }
 
-    // console.log(angle);
+    // console.log(angle,"angle");
     
     return angle
 
@@ -37,7 +42,7 @@ const ClockAngle=(hours,min)=>{
 // console.log(ClockAngle(3, 30));  // Output: 75 degrees
 // console.log(ClockAngle(12, 0));  // Output: 0 degrees
 console.log(ClockAngle(6, 45));  // Output: 90 degrees
-// console.log(ClockAngle(9, 5));
+console.log(ClockAngle(9, 5));
 
 // const result=ClockAngle(3,30)
 // console.log(result);
