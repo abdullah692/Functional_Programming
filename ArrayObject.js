0010a5e864586b19b901fc647a06dfbed5314f15
Abdullah Siddiqui <abdullahsidd37@gmail.com>
2025-02-11 18:31:36 +0500

    practice

--- ArrayObject.js
+++ ArrayObject.js
@@ -27,24 +27,21 @@ const convertDeliveries = (deliveries) => {
   // }
   // return obj;
 
-  let obj={}
-  let maxPackages=0;
-  let maxCity=null
-  for(let {city,packages} of deliveries)
-  {
-    if(!obj[city])
-    {
-      obj[city]={totalPackages:0}
+  let obj = {}
+  let maxPackages = 0;
+  let maxCity = null
+  for (let { city, packages } of deliveries) {
+    if (!obj[city]) {
+      obj[city] = { totalPackages: 0 }
     }
-    obj[city].totalPackages+=packages
+    obj[city].totalPackages += packages
 
-    if(obj[city].totalPackages > maxPackages)
-    {
-      maxPackages=obj[city].totalPackages
-      maxCity=city
+    if (obj[city].totalPackages > maxPackages) {
+      maxPackages = obj[city].totalPackages
+      maxCity = city
     }
   }
-  return {city:maxCity,...obj[maxCity]}
+  return { city: maxCity, ...obj[maxCity] }
   // return Object.entries(obj).filter(([key,value])=> Math.max(value))
 }
 
@@ -90,29 +87,26 @@ const playerFind = (player) => {
   // }
   // return { categories: obj, highestScorer: highestScore }
 
-  let obj={}
-  let highestScore=0
-  let maxLevel=null
-  for(let {name,score} of player)
-  {
-      let levels=score <= 500 ? "Beginner" :
-      score <=1500 ? "Intermediate" : "Advanced"
+  let obj = {}
+  let highestScore = 0
+  let maxLevel = null
+  for (let { name, score } of player) {
+    let levels = score <= 500 ? "Beginner" :
+      score <= 1500 ? "Intermediate" : "Advanced"
 
-      if(!obj[levels])
-      {
-        obj[levels]=[]
-      }
-      obj[levels].push(name)
+    if (!obj[levels]) {
+      obj[levels] = []
+    }
+    obj[levels].push(name)
 
-      if(score > highestScore)
-      {
-        highestScore=score
-        maxLevel=levels
-      }
+    if (score > highestScore) {
+      highestScore = score
+      maxLevel = levels
+    }
   }
-  console.log(obj,"levels");
-  return Object.fromEntries(Object.entries(obj).filter(([key,value])=> key == maxLevel))
-  
+  console.log(obj, "levels");
+  return Object.fromEntries(Object.entries(obj).filter(([key, value]) => key == maxLevel))
+
 }
 
 const val = playerFind(players)
@@ -152,24 +146,21 @@ const flightsRes = (flights) => {
   // }
   // return { flights: obj, highestFlights }
 
-  let obj={}
-  let highestFlights=null
-  let maxFlight=0
-  for(let {flight,airline,time} of flights)
-  {
-    if(!obj[airline])
-    {
-      obj[airline]=[]
+  let obj = {}
+  let highestFlights = null
+  let maxFlight = 0
+  for (let { flight, airline, time } of flights) {
+    if (!obj[airline]) {
+      obj[airline] = []
     }
     obj[airline].push(`${flight} (${time})`)
 
-    if(obj[airline].length > maxFlight)
-    {
-      maxFlight=obj[airline].length
-      highestFlights=airline
+    if (obj[airline].length > maxFlight) {
+      maxFlight = obj[airline].length
+      highestFlights = airline
     }
   }
-return {airline:highestFlights,  flights:obj[highestFlights]}  
+  return { airline: highestFlights, flights: obj[highestFlights] }
 }
 
 const resultFlight = flightsRes(flights)
@@ -208,30 +199,27 @@ const footPlayer = (players) => {
 
   // return {obj, maxGoals}
 
-  let obj={}
-  let highestGoals=0
-  let WinningTeam=null
-  for(let {name,team,goals} of players)
-  {
-    if(!obj[team])
-    {
-      obj[team]={players:[], totalGoals:0}
+  let obj = {}
+  let highestGoals = 0
+  let WinningTeam = null
+  for (let { name, team, goals } of players) {
+    if (!obj[team]) {
+      obj[team] = { players: [], totalGoals: 0 }
     }
-    
-    obj[team].players.push(name)
-    obj[team].totalGoals+=goals
 
-    if(obj[team].totalGoals > highestGoals)
-    {
-      highestGoals=obj[team].totalGoals
-      WinningTeam=team
+    obj[team].players.push(name)
+    obj[team].totalGoals += goals
+
+    if (obj[team].totalGoals > highestGoals) {
+      highestGoals = obj[team].totalGoals
+      WinningTeam = team
     }
-    
+
   }
   console.log(obj);
 
-  return {team:WinningTeam, goals:highestGoals, players:obj[WinningTeam].players}
-  
+  return { team: WinningTeam, goals: highestGoals, players: obj[WinningTeam].players }
+
 }
 const valFootlball = footPlayer(Footballplayers)
 console.log(valFootlball);
@@ -248,21 +236,31 @@ const books = [
 //   "George Orwell": ["1984", "Animal Farm"]
 // }
 
-const Books=(books)=>{
-  let obj={}
+const Books = (books) => {
+  // let obj={}
 
-  for(let val of books)
-  {
-    if(!obj[val.author])
-    {
-      obj[val.author]=[]
+  // for(let val of books)
+  // {
+  //   if(!obj[val.author])
+  //   {
+  //     obj[val.author]=[]
+  //   }
+  //   obj[val.author].push(val.title)
+  // }
+  // return Object.fromEntries(Object.entries(obj).sort((a,b)=>a.title-b.title))
+
+  let obj = {}
+  for (let { title, author } of books) {
+    if (!obj[author]) {
+      obj[author] = []
     }
-    obj[val.author].push(val.title)
+    obj[author].push(title)
   }
-  return Object.fromEntries(Object.entries(obj).sort((a,b)=>a.title-b.title))
+
+  return Object.fromEntries(Object.entries(obj).sort((a, b) => a.title - b.title))
 }
-const results=Books(books)
-console.log(results);
+const results = Books(books)
+console.log(results, "books");
 
 
 const posts = [
@@ -278,44 +276,56 @@ const posts = [
 //   "#health": ["Charlie"]
 // }
 
-const convertPost=(post)=>{
-  let obj={}
-  for(let {user,hashtags} of post)
-    {
-      for(let tags of hashtags)
-      {
-        if(!obj[tags])
-        {
-          obj[tags]=[]
-        }
-        obj[tags].push(user)
+const convertPost = (post) => {
+  // let obj={}
+  // for(let {user,hashtags} of post)
+  //   {
+  //     for(let tags of hashtags)
+  //     {
+  //       if(!obj[tags])
+  //       {
+  //         obj[tags]=[]
+  //       }
+  //       obj[tags].push(user)
+  //     }
+  //   } 
+  // return obj
+
+  let obj = {}
+  for (let { hashtags, user } of post) {
+    for (let tags of hashtags) {
+      if (!obj[tags]) {
+        obj[tags] = []
       }
-    } 
+      obj[tags].push(user)
+    }
+  }
   return obj
+
 }
-const reusltPost=convertPost(posts)
-console.log(reusltPost,"reusltPost");
+const reusltPost = convertPost(posts)
+console.log(reusltPost, "reusltPost");
 
 
 const transformToPosts = (hashtagMap) => {
-//   let userMap = {};
-// console.log(Object.entries(hashtagMap),"Object.entries(hashtagMap)");
+  //   let userMap = {};
+  // console.log(Object.entries(hashtagMap),"Object.entries(hashtagMap)");
 
-//   for (let [tag, users] of Object.entries(hashtagMap)) {
-//     console.log("tag",tag ,"users",users);
-    
-//     users.forEach(user => {
-//       if (!userMap[user]) {
-//         userMap[user] = [];
-//       }
-//       userMap[user].push(tag);
-//     });
-//   }
-//   console.log(userMap);
-//   return Object.entries(userMap).map(([user, hashtags]) => ({
-//     user,
-//     hashtags
-//   }));
+  //   for (let [tag, users] of Object.entries(hashtagMap)) {
+  //     console.log("tag",tag ,"users",users);
+
+  //     users.forEach(user => {
+  //       if (!userMap[user]) {
+  //         userMap[user] = [];
+  //       }
+  //       userMap[user].push(tag);
+  //     });
+  //   }
+  //   console.log(userMap);
+  //   return Object.entries(userMap).map(([user, hashtags]) => ({
+  //     user,
+  //     hashtags
+  //   }));
 
 
 }
@@ -326,5 +336,197 @@ const input = {
   "#health": ["Charlie"]
 };
 
-const values=transformToPosts(input)
+const values = transformToPosts(input)
 console.log(values);
+
+
+const movies = [
+  { title: "Inception", genre: "Sci-Fi" },
+  { title: "Interstellar", genre: "Sci-Fi" },
+  { title: "Titanic", genre: "Romance" },
+  { title: "The Notebook", genre: "Romance" },
+  { title: "Avengers", genre: "Action" }
+];
+
+// {
+//   "Sci-Fi": ["Inception", "Interstellar"],
+//   "Romance": ["Titanic", "The Notebook"],
+//   "Action": ["Avengers"]
+// }
+
+const moviesConvert = (movie) => {
+  let obj = {}
+  let genreCount = {}
+
+  for (let { title, genre } of movie) {
+    if (!obj[genre]) {
+      obj[genre] = []
+    }
+    obj[genre].push(title)
+    genreCount[genre] = (genreCount[genre] || 0) + 1
+
+  }
+  return { obj, genreCount }
+}
+
+const moviesResult = moviesConvert(movies)
+console.log(moviesResult);
+
+
+const peoples = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 25 },
+  { name: "David", age: 30 }
+];
+
+
+// {
+//   25: [{ name: "Alice", age: 25 }, { name: "Charlie", age: 25 }],
+//   30: [{ name: "Bob", age: 30 }, { name: "David", age: 30 }]
+// }
+
+const peopleGroup = (arr) => {
+  let obj = {}
+  for (let { name, age } of arr) {
+    if (!obj[age]) {
+      obj[age] = []
+    }
+    obj[age].push({ name, age })
+  }
+  return obj
+}
+
+const peopleResult = peopleGroup(peoples)
+console.log(peopleResult);
+
+//-----------------------------------------------------------------------
+
+const fruits = [
+  { name: "Apple", type: "Fruit" },
+  { name: "Carrot", type: "Vegetable" },
+  { name: "Banana", type: "Fruit" },
+  { name: "Spinach", type: "Vegetable" }
+];
+
+// { Fruit: 2, Vegetable: 2 }
+
+const fruitsOccurance = (fruits) => {
+  let obj = {}
+
+  for (let { name, type } of fruits) {
+
+    obj[type] = (obj[type] || 0) + 1
+
+  }
+  return obj
+}
+
+const resultFruits = fruitsOccurance(fruits)
+console.log(resultFruits);
+
+//..............................................................................
+
+const employees = [
+  { department: "HR", name: "Alice", age: 28, position: "Manager", salary: 60000 },
+  { department: "IT", name: "Bob", age: 25, position: "Developer", salary: 50000 },
+  { department: "HR", name: "Charlie", age: 30, position: "Recruiter", salary: 45000 },
+  { department: "IT", name: "David", age: 32, position: "System Admin", salary: 55000 },
+  { department: "Finance", name: "Eve", age: 29, position: "Accountant", salary: 48000 }
+];
+
+// {
+//   HR: { 
+//     Alice: { age: 28, position: "Manager", salary: 60000 }, 
+//     Charlie: { age: 30, position: "Recruiter", salary: 45000 } 
+//   },
+//   IT: { 
+//     Bob: { age: 25, position: "Developer", salary: 50000 }, 
+//     David: { age: 32, position: "System Admin", salary: 55000 } 
+//   },
+//   Finance: { 
+//     Eve: { age: 29, position: "Accountant", salary: 48000 } 
+//   }
+// }
+
+
+const employeesFetch = (employee) => {
+  let obj={}
+  for(let {department,name,age,position,salary} of employee)
+  {
+    if(!obj[department])
+    {
+        obj[department]={}
+    }
+    obj[department][name]={age,position,salary}
+
+  }
+  console.log(obj);
+  
+}
+
+const employeeResult = employeesFetch(employees)
+
+//-------------------------------------------------------------------------
+
+const data = [
+  { id: 1, value: 10 },
+  { id: 2, value: 15 },
+  { id: 1, value: 20 }
+];
+
+// { 1: { value: 30 }, 2: { value: 15 } }
+
+const mergeObj=(data)=>{
+  let obj={}
+  for(let {id,value} of data)
+  {
+    if(!obj[id])
+    {
+      obj[id]={value:0}
+    }
+    obj[id].value+=value
+  }
+  console.log(obj);
+  
+}
+
+const resultMerge=mergeObj(data)
+console.log(resultMerge);
+
+
+//---------------------------------------------------------------------------
+
+const orders = [
+  { customer: "John", item: "Laptop" },
+  { customer: "Jane", item: "Phone" },
+  { customer: "John", item: "Tablet" },
+  { customer: "Jane", item: "Phone" },
+  { customer: "John", item: "Phone" }
+];
+
+
+//Most purchased item: "Phone"
+
+const mostOrder=(orders)=>{
+  let obj={}
+  let mostItem=null
+  let itemCount=0
+
+  for(let {item} of orders)
+  {
+    obj[item]=(obj[item] || 0)+1
+    if(obj[item] > itemCount)
+    {
+      itemCount=obj[item]
+      mostItem=item
+    }
+  }
+  
+  return `Most purchased item:${mostItem} and its count is ${itemCount}`
+}
+
+const resultMost=mostOrder(orders)
+console.log(resultMost);
+
+
--- FlattenedArray.js
+++ FlattenedArray.js
@@ -1,8 +1,42 @@
 
-const FLattenedArray=(arr)=>{
-    const value=arr.flat(2);
+const FLattenedArray = (arr) => {
+    const value = arr.flat(2);
     console.log(value);
 }
 
-let arr=[1, [2, 3], [4, [5]]]
+let arr = [1, [2, 3], [4, [5]]]
 FLattenedArray(arr);
+
+
+function flattenArray(arr) {
+    //     let result = [];
+    //     let stack = [arr];
+    //   debugger
+    //     while (stack.length) {
+    //       let current = stack.shift();
+    //       console.log(current,"currents")// Take the first element
+    //       if (Array.isArray(current)) {
+    //         stack.unshift(...current); // Flatten step-by-step
+    //       } else {
+    //         result.push(current);
+    //       }
+    //     }
+    //     return result;
+
+    let result=[]
+    while(arr.length)
+    {
+        let current=arr.shift()
+        if(Array.isArray(current))
+        {
+            arr.unshift(...current)
+        }else{
+            result.push(current)
+        }
+
+    }
+return result
+
+}
+
+console.log(flattenArray([1, [2, [3, [4]], 5]])); // Expected Output: [1, 2, 3, 4, 5]
