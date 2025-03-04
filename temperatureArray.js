1db7527d6bc15e40a07382fd30813abbd93e067e
abdullah692 <abd.siddiqui987@gmail.com>
2025-03-05 00:28:35 +0500

    temperatureCheck

---
+++ temperatureArray.js
@@ -0,0 +1,30 @@
+let arr = [73, 74, 75, 71, 69, 72, 76, 73]
+
+const temperatureCheck = (arr) => {
+    let temp = []
+
+
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        let found = false
+        for (let j = i + 1; j < arr.length; j++) {
+            count++
+            if (arr[i] <= arr[j]) {
+                temp.push(count)
+                found = true
+                break;
+            }
+        }
+
+        if (!found) {
+            temp.push(0)
+        }
+    }
+
+
+    console.log(temp);
+
+}
+
+
+let result = temperatureCheck(arr)
