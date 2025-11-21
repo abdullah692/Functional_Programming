229d6e57932881fd81257ca0961c4e83eb2114d1
abdullah692 <abd.siddiqui987@gmail.com>
2025-11-21 23:21:03 +0500

    practice Nov21

--- cutStick.js
+++ cutStick.js
@@ -5,16 +5,28 @@ const cutStick = (arr) => {
     while (arr.some((v)=> v > 0)) {
         let minVal = Math.min(...arr.filter((v)=> v> 0))
         console.log(minVal);
+        count=0
 
-        for(let i=0; i< arr.length; i++)
+        for(let i=0; i < arr.length; i++)
         {
-            
+            if(arr[i] - minVal >= 0)
+            {
+
+                arr[i]=arr[i] - minVal
+                count++
+            }
         }
-        break;
+        console.log(arr,"arr");
+        stickOut.push(count)
         
     }
+    return stickOut
+    
 
 }
 
-let arr = [1, 2, 3.4, 3, 3, 2, 1]
+// let arr = [1, 2, 3, 4, 3, 3, 2, 1]
+let arr=[5 ,4 ,4 ,2 ,2 ,8 ]
+
 const result = cutStick(arr)
+console.log(result,"result");
